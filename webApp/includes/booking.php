dfd4850d8ab429a396d2a57c711bca4997204bb9
mnocerino <mnocerino@gmail.com>
2018-01-16 19:07:53 +0100

    First version of the booking date functions.

--- webApp/includes/booking.php
+++ webApp/includes/booking.php
@@ -16,9 +16,25 @@ function newBooking($userId, $roomId, $start, $end)
 
 function deleteBooking($bookingId)
 {
-
+    $dbConnection = dbConnect();
+    $query = "SELECT * from bookings where bookingId='$bookingId' LIMIT 1";
+    $rows = $dbConnection->query($query);
+    if ($rows->rowCount() > 0) {
+        foreach ($rows as $row) {
+            //TODO: Check if start date is less than 48 hours before now() in order to check if the user has rights to delete the booking or not.
+        }
+    }
 }
 
+function deleteBookingAsAdmin($bookingId)
+{
+    $dbConnection = dbConnect();
+    if (checkifUserIsAdmin(getUserIdFromSession())) {
+        $query = "DELETE from bookings where bookingId='$bookingId'";
+        $rows = $dbConnection->query($query);
+    }
+
+}
 function checkIfUserCanDelete($bookingId)
 {
     $dbConnection = dbConnect();
@@ -32,4 +48,21 @@ function checkIfUserCanDelete($bookingId)
         }
     }
     return false;
+}
+
+function getMondayOfWeek()
+{
+    return date('d-m-Y', strtotime('monday this week'));
+}
+
+function getSundayOfWeek()
+{
+    date('d-m-Y', strtotime('sunday this week'));
+}
+
+function getPreviousWeek($selectedMonday)
+{
+    return date('d-m-Y', strtotime("-7 days", strtotime("monday week", strtotime($selectedMonday))));
+    //TODO: THIS IS NOT WORKING.
+
 }
--- webApp/test.php
+++ webApp/test.php
@@ -5,7 +5,25 @@
  * Date: 14/01/2018
  * Time: 15:53
  */
-$userId = 1;
-$now = date('Y-m-d H:i:s', time());
-$query = "UPDATE users SET lastLogin = '$now' WHERE userId='$userId'";
-echo $query;
+
+require_once 'includes/booking.php';
+
+echo date('Y-m-d h:m:s', strtotime('monday this week'));
+echo "<br>";
+echo date('Y-m-d', strtotime('sunday this week'));
+echo "<br>";
+echo "Lunedi di questa settimana: ";
+echo getMondayOfWeek();
+echo "<br>";
+echo "Lunedì di settimana scorsa: ";
+echo getPreviousWeek(getMondayOfWeek());
+echo "<br>";
+echo "Lunedì di una settimana qualsiasi: ";
+$data = date("d-m-Y", strtotime('08-01-2018'));
+echo getPreviousWeek($data);
+
+
+$data = "12-05-2035";
+echo "<br>";
+echo "Lunedì del 12 5 2035: ";
+echo date('d-m-Y', strtotime('previous Monday', strtotime($data)));
