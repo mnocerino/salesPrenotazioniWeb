d0f98b3ee84ee2855f0d14ad350c217900fc73e0
Mattia Nocerino <mnocerino@gmail.com>
2018-02-05 18:39:24 +0100

    Minor changes.

--- db/salesprenotazioni (1).sql
+++ db/salesprenotazioni (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 31, 2018 alle 18:50
+-- Creato il: Feb 05, 2018 alle 18:39
 -- Versione del server: 10.1.28-MariaDB
 -- Versione PHP: 7.1.11
 
@@ -32,6 +32,14 @@ CREATE TABLE `administrators` (
   `userId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dump dei dati per la tabella `administrators`
+--
+
+INSERT INTO `administrators` (`userId`) VALUES
+(13),
+(17);
+
 -- --------------------------------------------------------
 
 --
@@ -52,7 +60,11 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`bookingId`, `roomId`, `userId`, `status`, `start`, `end`) VALUES
-(16, 1, 1, 1, '2018-01-24 09:15:00', '2018-01-24 12:00:00');
+(1, 1, 13, 1, '2018-02-09 08:00:00', '2018-02-09 09:00:00'),
+(2, 1, 13, 2, '2018-02-08 08:00:00', '2018-02-08 14:00:00'),
+(3, 2, 14, 1, '2018-02-07 12:00:00', '2018-02-07 14:00:00'),
+(4, 2, 16, 1, '2018-02-07 17:00:00', '2018-02-07 19:00:00'),
+(5, 4, 17, 1, '2018-02-07 08:00:00', '2018-02-07 12:00:00');
 
 -- --------------------------------------------------------
 
@@ -64,16 +76,18 @@ CREATE TABLE `rooms` (
   `roomId` int(11) NOT NULL,
   `roomName` varchar(255) NOT NULL,
   `roomDescription` varchar(1000) NOT NULL,
-  `roomSpace` int(11) NOT NULL
+  `isActive` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `rooms`
 --
 
-INSERT INTO `rooms` (`roomId`, `roomName`, `roomDescription`, `roomSpace`) VALUES
-(1, 'Ufficio 1', 'Ufficio di 20mq', 3),
-(2, 'Ufficio 2', 'Ufficio di 15mq', 2);
+INSERT INTO `rooms` (`roomId`, `roomName`, `roomDescription`, `isActive`) VALUES
+(1, 'Sala 1', 'Piccola sala', 1),
+(2, 'Sala 2', 'Grande sala', 1),
+(3, 'Sala psicomotoria', 'Sala attrezzata', 1),
+(4, 'Pet therapy', 'Area per attivitÃ  con animali', 1);
 
 -- --------------------------------------------------------
 
@@ -93,7 +107,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`sessionId`, `userId`, `start`, `end`) VALUES
-('tv1e0cj4mfsgjb4ffuk5od34kk', 1, '2018-01-31 11:08:45', '2018-01-31 12:08:45');
+('hjru5m0asbiqnngrtmsl77mqt6', 13, '2018-02-05 17:36:36', '2018-02-05 18:36:36');
 
 -- --------------------------------------------------------
 
@@ -109,8 +123,8 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1',
   `lastLogin` datetime DEFAULT NULL,
-  `rate` float NOT NULL,
-  `allowance` int(11) NOT NULL
+  `rate` float NOT NULL DEFAULT '20',
+  `allowance` int(11) NOT NULL DEFAULT '20'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -118,7 +132,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `name`, `surname`, `mail`, `password`, `status`, `lastLogin`, `rate`, `allowance`) VALUES
-(1, 'Mattia', 'Nocerino', 'mnocerino@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, '2018-01-31 12:08:45', 11.48, 30);
+(13, 'Mattia', 'Nocerino', 'mnocerino@gmail.com', '955a5c1dce63a9cc15edaa67b82b3e6336a79b77', 1, '2018-02-05 18:36:36', 20, 20),
+(14, 'Tizio', 'Caio', 'tizio@caio.com', 'ff96221d843e4e52b69e525bcbdff613d4afc297', 1, NULL, 20, 20),
+(15, 'Mario', 'Rossi', 'mario@rossi.com', 'acac2c00fbf71543c326df7d80897a5804bee9c7', 1, NULL, 25.64, 14),
+(16, 'Giulio', 'Bianchi', 'giulio@bianchi.com', '450f4fa44f39412f9cff91999ac5f4699273113b', 0, NULL, 20, 20),
+(17, 'Giancarlo', 'Ruffo', 'giancarlo@ruffo.com', '70ccd9007338d6d81dd3b6271621b9cf9a97ea00', 1, '2018-02-05 18:32:50', 18.2, 50),
+(18, 'Tes', 'test', 'test@test.com', '67e28b4e1799f71798322c6e0f486a3eb2d2680d', 1, NULL, 20, 20);
 
 --
 -- Indici per le tabelle scaricate
@@ -156,6 +175,7 @@ ALTER TABLE `sessions`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `mail` (`mail`),
   ADD KEY `userId` (`userId`);
 
 --
@@ -166,19 +186,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT per la tabella `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT per la tabella `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `roomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `roomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Limiti per le tabelle scaricate
